2dfd20b648c2d381595135ee1356c5fd618103dc
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-21 14:19:08 +0200

    new

--- script/script.js
+++ script/script.js
@@ -24,22 +24,22 @@
 
 
 
-const startButton = document.querySelector('main button');
+// const startButton = document.querySelector('main button');
 const captionContainer = document.querySelector('main section div');
 const video = document.querySelector('video');
 
-console.log(startButton,captionContainer,video);
+console.log(captionContainer,video);
 
-startButton.addEventListener('click', () => {
+// startButton.addEventListener('click', () => {
     if (video.pause) {
         video.play();
-        startButton.classList.add('hidden');
+        // startButton.classList.add('hidden');
         captionContainer.classList.add('start');
         console.log("start");
     } else {
         video.pause();
-        startButton.classList.remove('hidden');
+        // startButton.classList.remove('hidden');
         captionContainer.classList.remove('start');
         console.log("pause");
     }
-});
+// });
