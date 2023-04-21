0528496b77d155d931fe88195354eea4534eabbe
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-21 14:04:12 +0200

    new

---
+++ script/coptions.js
@@ -0,0 +1,81 @@
+export let captions = [
+    {caption: 'Ebra! boterhammen maken!', time:{start: '0ms', end: '2ms'}},
+    {caption: 'Stop niet met het maken van sandwiches!', time:{start: '2ms', end: '4ms'}},
+    {caption: 'Ja, chef-kok', time:{start: '4ms', end: '4.5ms'}},
+    {caption: 'Ik ga drie secties maken, oké?', time:{start: '4.5ms', end: '6ms'}},
+    {caption: 'Ze worden Wet, Hot en sweet, oké?', time:{start: '7ms', end: '8ms'}},
+    {caption: 'Ik ga groene tape nemen, die secties maken.', time:{start: '9ms', end: '10ms'}},
+  ];
+  
+
+// export let captions = [
+//     caption = 'Ebra! boterhammen maken!',
+//     caption = 'Stop niet met het maken van sandwiches!',
+//     caption[2] = 'Ja, chef-kok',
+//     caption[3] = 'Ik ga drie secties maken, oké?',
+//     caption[4] = 'Ze worden Wet, Hot en sweet, oké?',
+//     caption[5] = 'Ik ga groene tape nemen, die secties maken.',
+//     caption[6] = '',
+
+//     caption[7] = '',
+
+//     caption[8] = '',
+
+//     caption[9] = '',
+
+//     caption[1'0ms'] = '',
+
+//     caption[11] = '',
+
+//     caption[12] = '',
+
+//     caption[13] = '',
+
+//     caption[14] = '',
+
+//     caption[15] = '',
+
+//     caption[16] = '',
+
+//     caption[17] = '',
+
+//     caption[18] = '',
+
+//     caption[19] = '',
+
+//     caption[2'0ms'] = '',
+
+//     caption[21] = '',
+
+//     caption[22] = '',
+
+//     caption[23] = '',
+
+//     caption[24] = '',
+
+//     caption[25] = '',
+
+//     caption[26] = '',
+
+//     caption[27] = '',
+
+//     caption[28] = '',
+
+//     caption[29] = '',
+
+//     caption[3'0ms'] = '',
+
+//     caption[31] = '',
+
+//     caption[32] = '',
+
+//     caption[33] = '',
+
+//     caption[34] = '',
+
+//     caption[35] = '',
+
+//     caption[36] = '',
+
+//     caption[37] = ''
+// ]
---
+++ script/script.js
@@ -0,0 +1,45 @@
+// import { captions } from "./coptions.js";
+
+// const startButton = document.querySelector('main button')
+// const captionContainer = document.querySelector('main section div')
+// const video = document.querySelector('video')
+
+
+// startButton.addEventListener('click', () => {
+//     if (video.pause) {
+//         video.play();
+//         startButton.classList.add('hidden')
+//         captionContainer.classList.add('start')
+
+//         console.log("start");
+//     } else {
+//         video.pause()
+//         startButton.classList.remove('hidden')
+//         captionContainer.classList.remove('start')
+
+//         console.log("pause");
+//     }
+// })
+
+
+
+
+const startButton = document.querySelector('main button');
+const captionContainer = document.querySelector('main section div');
+const video = document.querySelector('video');
+
+console.log(startButton,captionContainer,video);
+
+startButton.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        startButton.classList.add('hidden');
+        captionContainer.classList.add('start');
+        console.log("start");
+    } else {
+        video.pause();
+        startButton.classList.remove('hidden');
+        captionContainer.classList.remove('start');
+        console.log("pause");
+    }
+});
